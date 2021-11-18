c75a6d9d660a6d7f040c7fd0522586f773db35a5
DylanAllmon <93544645+DylanAllmon@users.noreply.github.com>
2021-11-18 12:37:47 -0800

    Update Jumper.js

--- jumper/Jumper.js
+++ jumper/Jumper.js
@@ -1,13 +1,20 @@
 function setup() {
 	createCanvas(windowW, windowH, WEBGL);
+	textFont(font);
+  textSize(ratio*40);
+  textAlign(CENTER, CENTER);
 	player.color = color(80,0,200);
 	angleMode(DEGREES);
+	obstacles.addColumn();
+	obstacles.addColumn();
 	obstacles.addColumn();
 	obstacles.addColumn();
 	cam = createCamera();
 	distance = cam.eyeZ;
 	perspective(80);
 	cam.setPosition(mainOffset.x*blockSize,-mainOffset.y*blockSize,mainOffset.z*blockSize);
+	
+	txtr = createGraphics(blockSize,blockSize);
 	
 	strokeWeight(0);
 }
@@ -51,7 +58,18 @@ function render() {
 	ambientLight(brightness);
 	
 	pointLight(color(255), 0,0,mainOffset.z*blockSize)
+	let e = hue+maxHue/2;
 	
+	if (e > maxHue) {
+		e-=maxHue;
+	}
+	push();
+	let a = atan2(0+cam.eyeX,cam.eyeZ-sideOffset.z*blockSize);
+	translate(0,0,sideOffset.z*blockSize);
+  rotateY(a);
+	fill(color(('hsl(' + floor(e*(360/maxHue)) + ',100%,30%)')));
+	text("Score: " + score, 0, -150);
+	pop();
 	renderPlayer();
 	renderPlatform();
 	renderObstacles();
@@ -64,10 +82,22 @@ function tickSecond() {
 }
 
 function leveling() {
-	if (ticks % spawnRate == 0) {
+	if (ticks == spawnRate/2) {
+		let r = round(random(-1,1));
+		let e = round(random(1,5));
+		if (r == 0) {
+			e = round(random(4,5));
+		}
+		obstacle((r*4+(-r)*(e/2)),0,40,e,round(random(1,3)),1,count);
+	}
+	if (ticks>=spawnRate) {
+		ticks = 0;
 		let e = round(random(1,5));
 		let r = round(random(-1,1));
-		obstacle((r*4+(-r)*(e/2)),0,40,e,round(random(1,3)),1);
+		if (r == 0) {
+			e = round(random(4,5));
+		}
+		obstacle((r*4+(-r)*(e/2)),0,40,e,round(random(1,3)),1,count);
 	}
 }
 
