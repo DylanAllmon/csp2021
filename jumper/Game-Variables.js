ac5099e4bd363e5325a9cd5630a6e99c6ee20c30
DylanAllmon <93544645+DylanAllmon@users.noreply.github.com>
2021-12-07 13:00:11 -0800

    Update Game-Variables.js

--- jumper/Game-Variables.js
+++ jumper/Game-Variables.js
@@ -101,7 +101,7 @@ function preload() {
 		Helvetica = loadFont("Font.ttf");
 	} else {
 		Helvetica = loadFont("jumper/Font.ttf");
-		Version = loadStrings("jumper/Version.txt")[0];
+		Version = loadStrings("jumper/Version.txt");
 	}
 }
 
